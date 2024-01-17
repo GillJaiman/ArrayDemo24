a1959b3dc27ef67b68b33f39a12d1d89d00c7bbc
Jaimanjot Kaur Gill <gilljaim@sheridancollege.ca>
2024-01-17 09:59:54 -0500

    Third Commit: Added print in studentList

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -14,7 +14,8 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        studentList[0]=s1;
+        System.out.println("Third Commit");
     }
     
 }
