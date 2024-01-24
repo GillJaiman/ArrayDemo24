04b4340b5cf7362453a897f7db979828e3d0fdef
Jaimanjot Kaur Gill <gilljaim@sheridancollege.ca>
2024-01-24 10:21:56 -0500

    added part time student class

---
+++ src/helloworld/partTimeStudent.java
@@ -0,0 +1,33 @@
+package helloworld;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author theau
+ */
+public class partTimeStudent extends HelloWorld
+{
+    private int numOfCourses;
+
+    public partTimeStudent(int numOfCourses) {
+        this.numOfCourses = numOfCourses; 
+        
+    }
+
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+    
+    
+    
+    
+    
+}
